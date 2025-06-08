7c1eab0d79334285549a4158959415b529ce25d5
Navido97 <65952173+Navido97@users.noreply.github.com>
2025-06-08 13:38:27 -0500

    Update main.js

--- front_end/js/main.js
+++ front_end/js/main.js
@@ -1,1 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const csvUrl = "https://soccer-data-bucket-nav.s3.amazonaws.com/data/bundesliga_standings_2022.csv";
 
+  Papa.parse(csvUrl, {
+    download: true,
+    header: true,
+    complete: function (results) {
+      const data = results.data;
+
+      const columns = Object.keys(data[0]).map(key => ({
+        title: key,
+        data: key
+      }));
+
+      $('#standings').DataTable({
+        data: data,
+        columns: columns
+      });
+    }
+  });
+});
